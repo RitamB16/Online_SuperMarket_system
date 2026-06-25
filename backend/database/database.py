eb7f454b87212fda46cf424bae4cfda23b4a0acd
RitamB16 <ritambera6969@gmail.com>
2026-06-25 22:20:53 +0530

    Secured database URL with dotenv

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,16 +1,20 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-# Replace 'root' and 'password' with your actual MySQL credentials
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:16102005@localhost:3306/supermarket_db"
+
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency to get a database session for our API routes
 def get_db():
     db = SessionLocal()
     try:
